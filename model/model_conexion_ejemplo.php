1c1b02218e59c280dbf2e5bea3bad82251b559cf
Jersson <jerry_14_07@hotmail.com>
2026-06-06 15:56:28 -0500

    se creo conexión ejemplo

--- model/model_conexion_ejemplo.php
+++ model/model_conexion_ejemplo.php
@@ -1,13 +1,13 @@
-﻿<?php
+<?php
 class conexionBD{
     private $pdo;
 
     public function conexionPDO(){
         $host       = "localhost";
-        $usuario    = "root";
-        $contrasena = "";
-        $bdName     = "grados_titulos";
-        $port       = "3307"; // Ajusta el puerto aquí
+        $usuario    = "TU_USUARIO";
+        $contrasena = "TU_CONTRASENA";
+        $bdName     = "TU_BASE_DE_DATOS";
+        $port       = "3306"; // Ajusta el puerto según tu entorno
         try {
             $this->pdo = new PDO("mysql:host=$host;port=$port;dbname=$bdName", $usuario, $contrasena);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
